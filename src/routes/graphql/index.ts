5943378fe8630615d272e90ec7fc7f64fd7401f1
Andrei Paleshka <andrepoleshko@gmail.com>
2024-03-04 16:53:28 +0100

    feat: implement graphql schema and handler

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { GraphQLSchema, graphql } from 'graphql';
+import { Query } from './types/query.js';
+import { prisma } from './types/prisma.js';
+import { Mutation } from './types/mutation.js';
+
+const schema = new GraphQLSchema({
+	query: Query,
+  mutation: Mutation
+});
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -12,8 +21,20 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
-      return {};
+    async handler(req, _reply) {
+      const { query, variables } = req.body;
+
+      const { data, errors } = await graphql({
+        schema,
+        source: query,
+        variableValues: variables,
+        contextValue: prisma
+      });
+
+      return {
+        data,
+        errors
+      }
     },
   });
 };
